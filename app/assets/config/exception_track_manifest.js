5482a0368c1302a1561f30350b81ba5d944da45e
gh12128 <1137190810@qq.com>
2018-05-18 14:58:35 +0800

    Update exception_track_manifest.js
    update link_directory

--- app/assets/config/exception_track_manifest.js
+++ app/assets/config/exception_track_manifest.js
@@ -1,2 +1,3 @@
-//= link_directory ../javascripts/exception-track.js
-//= link_directory ../stylesheets/exception-track.css
+// link_directory ../javascripts/exception-track.js
+// link_directory ../stylesheets/exception-track.css
+//= link_directory ../stylesheets
